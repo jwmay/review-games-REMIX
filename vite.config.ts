aad1eb0703ef0a884cfa27b7f65509d575dcb64d
Joseph May <jwmay87@gmail.com>
2024-11-30 13:33:37 -0800

    Fix basename

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
-    v3_singleFetch: true;
+    v3_singleFetch: true
   }
 }
 
 export default defineConfig({
+  base: '/review-games/',
   plugins: [
     remix({
+      basename: '/review-games/',
       ssr: false,
       future: {
         v3_fetcherPersist: true,
@@ -22,4 +24,4 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-});
+})
